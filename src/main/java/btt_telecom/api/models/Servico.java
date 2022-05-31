c96f4bea14a1a653b35c9ca99191cd506d80cb91
cagiiap <carjuniorrj@hotmail.com>
2022-05-30 23:03:22 -0300

    first commit

---
+++ src/main/java/btt_telecom/api/models/Servico.java
@@ -0,0 +1,144 @@
+package btt_telecom.api.models;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Servico {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_Sequence")
+    @SequenceGenerator(name = "id_Sequence", sequenceName = "ID_SEQ")
+	private Long id;
+	
+	@OneToOne(cascade = CascadeType.MERGE)
+	private Cliente cliente;
+	
+	@OneToOne(cascade = CascadeType.MERGE)
+	private Funcionario funcionario;
+	
+	private String contrato;
+	
+	private String protocolo;
+	
+	private String descricao;
+	
+	private String materiais; 
+	
+	private String status;
+	
+	private String observacoes;
+	
+	private String cod_quebra;
+	
+	private Date data_inicio;
+	
+	private Date data_finalizacao;
+
+	public Servico() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public String getContrato() {
+		return contrato;
+	}
+
+	public void setContrato(String contrato) {
+		this.contrato = contrato;
+	}
+
+	public String getProtocolo() {
+		return protocolo;
+	}
+
+	public void setProtocolo(String protocolo) {
+		this.protocolo = protocolo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getMateriais() {
+		return materiais;
+	}
+
+	public void setMateriais(String materiais) {
+		this.materiais = materiais;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public String getCod_quebra() {
+		return cod_quebra;
+	}
+
+	public void setCod_quebra(String cod_quebra) {
+		this.cod_quebra = cod_quebra;
+	}
+
+	public Date getData_inicio() {
+		return data_inicio;
+	}
+
+	public void setData_inicio(Date data_inicio) {
+		this.data_inicio = data_inicio;
+	}
+
+	public Date getData_finalizacao() {
+		return data_finalizacao;
+	}
+
+	public void setData_finalizacao(Date data_finalizacao) {
+		this.data_finalizacao = data_finalizacao;
+	}
+}
