4b61708fa173c3811e5b25f870b4b85ca4b6b204
Carlos Roberto <carlos.roberto@t2s.com.br>
2022-11-05 12:20:32 -0300

    add field data

--- src/main/java/btt_telecom/api/modules/servico/dto/ServicoDTO.java
+++ src/main/java/btt_telecom/api/modules/servico/dto/ServicoDTO.java
@@ -20,6 +20,8 @@ public class ServicoDTO {
 	private String servico;
 	
 	private String status;
+	
+	private String data;
 		
 	private String hora_finalizacao;
 
@@ -33,6 +35,7 @@ public class ServicoDTO {
 		this.status = servico.getStatus();
 		this.provedor = servico.getProvedor().getName();
 		this.servico = servico.getServicoProvedor().getServico();
+		this.data = servico.getData().toString();
 		if(servico.getHora_finalizacao() != (null)) {
 			this.hora_finalizacao = servico.getHora_finalizacao().toString();
 		}
@@ -42,76 +45,84 @@ public class ServicoDTO {
 		return id;
 	}
 
-	public String getFuncionario() {
-		return funcionario;
-	}
-
-	public String getEmpresa() {
-		return empresa;
-	}
-
-	public String getCliente() {
-		return cliente;
-	}
-
-	public String getContrato() {
-		return contrato;
-	}
-
-	public String getProtocolo() {
-		return protocolo;
-	}
-
-	public String getProvedor() {
-		return provedor;
-	}
-
-	public String getServico() {
-		return servico;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public String getHora_finalizacao() {
-		return hora_finalizacao;
-	}
-
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getFuncionario() {
+		return funcionario;
 	}
 
 	public void setFuncionario(String funcionario) {
 		this.funcionario = funcionario;
 	}
 
+	public String getEmpresa() {
+		return empresa;
+	}
+
 	public void setEmpresa(String empresa) {
 		this.empresa = empresa;
+	}
+
+	public String getCliente() {
+		return cliente;
 	}
 
 	public void setCliente(String cliente) {
 		this.cliente = cliente;
 	}
 
+	public String getContrato() {
+		return contrato;
+	}
+
 	public void setContrato(String contrato) {
 		this.contrato = contrato;
+	}
+
+	public String getProtocolo() {
+		return protocolo;
 	}
 
 	public void setProtocolo(String protocolo) {
 		this.protocolo = protocolo;
 	}
 
+	public String getProvedor() {
+		return provedor;
+	}
+
 	public void setProvedor(String provedor) {
 		this.provedor = provedor;
+	}
+
+	public String getServico() {
+		return servico;
 	}
 
 	public void setServico(String servico) {
 		this.servico = servico;
 	}
 
+	public String getStatus() {
+		return status;
+	}
+
 	public void setStatus(String status) {
 		this.status = status;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHora_finalizacao() {
+		return hora_finalizacao;
 	}
 
 	public void setHora_finalizacao(String hora_finalizacao) {
