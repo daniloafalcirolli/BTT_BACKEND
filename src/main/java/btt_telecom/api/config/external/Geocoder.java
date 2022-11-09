24956d38c3f87ec23493f9969551567561feed3c
Carlos Roberto <carlos.roberto@t2s.com.br>
2022-11-09 20:48:01 -0300

    sou um merda

--- src/main/java/btt_telecom/api/config/external/Geocoder.java
+++ src/main/java/btt_telecom/api/config/external/Geocoder.java
@@ -5,7 +5,6 @@ import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 
 import org.json.JSONArray;
@@ -16,7 +15,6 @@ public class Geocoder {
 
     private static final String GEOCODING_RESOURCE = "https://maps.googleapis.com/maps/api/geocode/json";
     private static final String API_KEY = "AIzaSyD1j-aZgDYi3Wq7Na29lk45otM22aYF8uM";
-    private static String responseBody = "";
 
     public JSONObject GeocodeSync(String query) throws IOException, InterruptedException {
         HttpClient httpClient = HttpClient.newHttpClient();
@@ -24,16 +22,13 @@ public class Geocoder {
         String encodedQuery = URLEncoder.encode(query,"UTF-8");
         String requestUri = GEOCODING_RESOURCE + "?key=" + API_KEY + "&address=" + encodedQuery;
 
-        HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri)).build();
-        
-        httpClient.sendAsync(geocodingRequest, BodyHandlers.ofString())
-        		.thenApply(HttpResponse::body)
-        		.thenAccept(response -> responseBody = response);
-        
-//        HttpResponse<?> geocodingResponse = httpClient.send(geocodingRequest,
-//                HttpResponse.BodyHandlers.ofString());
+        HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri))
+                .timeout(Duration.ofMillis(600)).build();
+
+        HttpResponse<?> geocodingResponse = httpClient.send(geocodingRequest,
+                HttpResponse.BodyHandlers.ofString());
         try {
-			JSONObject json = new JSONObject(responseBody);
+			JSONObject json = new JSONObject(geocodingResponse.body().toString());
 			JSONArray jarr = json.getJSONArray("results");
 			JSONObject geometry = new JSONObject(jarr.get(0).toString());
 			JSONObject location = new JSONObject(geometry.get("geometry").toString());
