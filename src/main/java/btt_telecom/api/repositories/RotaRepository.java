c9c2b960a81ec2da2a43531aaf911b73f531ff4b
Carlos Roberto <carlos.roberto@t2s.com.br>
2022-11-10 11:56:59 -0300

    fix query

--- src/main/java/btt_telecom/api/repositories/RotaRepository.java
+++ src/main/java/btt_telecom/api/repositories/RotaRepository.java
@@ -11,7 +11,7 @@ public interface RotaRepository extends JpaRepository<Rota, Long>{
 	@Query(value = "select * from rotas where id_servico = ?1", nativeQuery = true)
 	Rota findRotaById_serv(Long id_servico);
 	
-	@Query(value = "SELECT * FROM rotas where r.CPF_FUNCIONARIO = '?1' and r.\"DATA\" >= to_date(?2, 'yyyy-MM-dd') AND r.\"DATA\" <= to_date(?3, 'yyyy-MM-dd') order by data, hora", nativeQuery = true)
+	@Query(value = "SELECT * FROM rotas r where r.CPF_FUNCIONARIO = ?1 and r.\"DATA\" >= to_date(?2, 'yyyy-MM-dd') AND r.\"DATA\" <= to_date(?3, 'yyyy-MM-dd') order by data, hora", nativeQuery = true)
 	List<Rota> findAllByFuncInInterval(String cpf, String data_inicio, String data_fim);
 	
 	@Query(value = "select * from rotas WHERE data = to_date(?1, 'yyyy-MM-dd') AND funcionario_id = ?2 order by hora asc", nativeQuery = true)
